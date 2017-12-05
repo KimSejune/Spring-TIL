7c7c1d8723011c1bd2f8f32e6aa0e2a3bb396f5e
kimsejune <tpwns1088@gmail.com>
2017-12-05 21:11:54 +0900

    Redirect Example

---
+++ 7day/redirectExample/src/main/java/com/java/re/RedirectController.java
@@ -0,0 +1,34 @@
+package com.java.re;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RedirectController {
+
+	@RequestMapping( value = "/studentConfirm")
+	public String studentRedirect(HttpServletRequest httpServletRequest, Model model) {
+		
+		String hid = httpServletRequest.getParameter("id");
+		model.addAttribute("mid", hid);
+		if(hid.equals("abc")) {
+			return "redirect:studentOk";
+		}
+		
+		return "redirect:studentNo";
+	}
+	
+	@RequestMapping("/studentOk")
+	public String studentOk() {
+		return "student/studentOk";
+	}
+	
+	@RequestMapping("/studentNo")
+	public String studentNo() {
+		return "student/studentNo";
+	}
+	
+}
