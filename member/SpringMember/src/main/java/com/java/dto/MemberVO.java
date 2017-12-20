0b363d2268f16d0bd1df11d8f763e1d1e42da81c
kimsejune <tpwns1088@gmail.com>
2017-12-20 11:08:25 +0900

    insert 작성

---
+++ member/SpringMember/src/main/java/com/java/dto/MemberVO.java
@@ -0,0 +1,28 @@
+package com.java.dto;
+
+public class MemberVO {
+	private int id;
+	private String pw;
+	private String name;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
